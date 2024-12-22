16e1d494e542e36bbfa90a2bf8477ece5ba98495
prabal864 <940pps@gmail.com>
2024-12-22 13:28:35 +0530

    Adding DockerFile

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 connectToDatabase()
     .then(()=> {
-        app.listen(PORT, () => console.log("Server Open & Connected To Database"));
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
     })
     .catch((err)=>console.log(err));
     
