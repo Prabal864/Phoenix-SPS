a91b0ebacfa51ec51191e92a7c81708887624d68
prabal864 <940pps@gmail.com>
2024-12-23 01:07:48 +0530

    Token Config

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -8,6 +8,7 @@ export const createToken = (id:string, email: string,expiresIn:string) => {
     const token = jwt.sign(payload,process.env.JWT_SECRET, {
         expiresIn:expiresIn,
     });
+    
     return token;
 };
 
