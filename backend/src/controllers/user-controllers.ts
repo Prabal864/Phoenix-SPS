de969e37a866a0b6994319c1ad579707e1d041ef
prabal864 <940pps@gmail.com>
2024-12-23 00:46:55 +0530

    updating

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -7,8 +7,8 @@ import { COOKIE_NAME } from "../utils/constants.js";
 export const getAllUsers = async (req:Request, res:Response, next:NextFunction) =>{
     try {       
         const users = await User.find();
-
-        return res.status(200).json({message:"OK", users});
+        return res.status(200).json({message:"OK",
+             users});
     } catch (error) {
         console.log(error);
         return res.status(200).json({message:"ERROR", cause : error.message});
